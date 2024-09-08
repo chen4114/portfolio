5c14bfbfde85f0f0e3e973a26b0c10392775269a
chen4114 <chen4114@purdue.edu>
2024-09-08 17:34:25 -0400

    add download resume

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,13 +2,12 @@
 "use client"
 
 import { usePathname } from 'next/navigation';
-// import { useRouter } from 'next/navigation';
 import React from 'react';
+import { FaDownload } from 'react-icons/fa'
 
 const Nav = () => {
-    // const router = useRouter();
     const pathname = usePathname();
-    // console.log(router)
+    const resumePath = `${process.env.NEXT_PUBLIC_BASE_PATH}/files/Resume_Yuheng Chen_Purdue_ECE_PhD.pdf`
 
     const links = [
         { title: "Home", url: `${process.env.NEXT_PUBLIC_BASE_PATH}/` },
@@ -21,17 +20,29 @@ const Nav = () => {
         <nav className='bg-white py-4'>
             <ul className='flex justify-center space-x-10'>
                 {links.map((link, index) => (
-                    <li key={index} className={`text-gray-800 hover:text-black font-medium ${
-                        pathname === link.url ? 'text-blue-500 underline decoration-sky-500 underline-offset-4' : ''
-                    }`}>
+                    <li key={index} className={`text-gray-800 hover:text-black font-medium ${pathname === link.url ? 'text-blue-500 underline decoration-sky-500 underline-offset-4' : ''
+                        }`}>
                         <a href={link.url} className="text-sm uppercase tracking-wider">
                             {link.title}
                         </a>
                     </li>
                 ))}
+                <a
+                    href={`${resumePath}`}
+                    download
+                    className='flex items-center justify-center w-[12rem] h-[1.8rem] bg-gray-400 hover:bg-gray-600 text-white rounded-lg hover:bg-gray-700'
+                >
+                    <FaDownload className='mr-2' />
+                    Download Resume
+                </a>
             </ul>
+
+
         </nav>
     );
 };
 
 export default Nav;
+
+
+
